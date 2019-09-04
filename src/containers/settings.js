0fb62ddda0b4912de510df3ecf8464af8d2d865e
Alastair Porter <alastair.porter@upf.edu>
2019-09-04 16:32:28 +0200

    Configuration to mount app at a different location

--- src/containers/settings.js
+++ src/containers/settings.js
@@ -1,3 +1,5 @@
 
 export const GRAPHQL_HOST = "http://localhost:4000/";
-export const APP_LOCATION = "http://localhost:4000/";
+
+export const APP_PATH = "";
+export const APP_LOCATION = GRAPHQL_HOST + APP_PATH;
--- src/index.js
+++ src/index.js
@@ -8,13 +8,14 @@ import { Router, Route, browserHistory } from 'react-router'
 
 import { reducers } from 'meld-clients-core/src/reducers';
 import DigitalScoreEdition from './containers/digitalScoreEdition';
+import {APP_PATH} from "./containers/settings";
 
 const createStoreWithMiddleware = applyMiddleware(thunk, ReduxPromise)(createStore);
 
 ReactDOM.render(
 	<Provider store={createStoreWithMiddleware(reducers)}>
 		<Router history={browserHistory}> 
-			<Route path="/" component={DigitalScoreEdition} />
+			<Route path={`/${APP_PATH}`} component={DigitalScoreEdition} />
 		</Router>
 	</Provider>
 		, document.querySelector('.container')
